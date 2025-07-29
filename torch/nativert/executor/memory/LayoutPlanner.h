867a7dba364e5651786a11c1d4d945a89efc4bde
Zhengxu Chen <zhxchen17@meta.com>
2025-07-28 21:43:14 -0700

    [nativert] Add OSS version of ModelRunner (#159268)
    Summary:
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/159268
    
    Implement a ModelRunner from scratch with the minimum features for OSS only
    
    Test Plan:
    test_export -r NativeRT
    
    Rollback Plan:
    
    Reviewed By: dolpm
    
    Differential Revision: D78979812

--- torch/nativert/executor/memory/LayoutPlanner.h
+++ torch/nativert/executor/memory/LayoutPlanner.h
@@ -51,6 +51,9 @@ class LayoutPlanner {
           kernelSchemas,
       const std::vector<bool>& persistentValues,
       const torch::nativert::LayoutPlannerSettings& settings);
+#if !defined(_MSC_VER)
+  TORCH_API // TODO Doesn't work on msvc.
+#endif
   ~LayoutPlanner();
 
   LayoutPlanner(LayoutPlanner&& other) = delete;
